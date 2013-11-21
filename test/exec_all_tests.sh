972b32b4c6ac9d195cba2bee8e4a1f80d86e265c
Satoshi Tajima <Satoshi_Tajima@voyagegroup.com>
2013-11-21 09:43:28 +0900

    support simple test.

--- test/exec_all_tests.sh
+++ test/exec_all_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 cd "./test/"
 
+COLOR=$1
+
 TESTS=(\
 "test_libs_common.rb" \
 "test_libs_cloudformation.rb" \
@@ -11,9 +13,15 @@ TESTS=(\
 echo "==== TEST START ====="
 
 STATUS=0
+
+TEST_COM='rg'
+if [ -z ${COLOR} ];then
+	TEST_COM='ruby'
+fi
+
 for TEST in ${TESTS[@]}; do
-	bundle exec rg $TEST
-  if [ $? = 1 ];then STATUS=1; fi
+	bundle exec $TEST_COM $TEST
+	if [ $? = 1 ];then STATUS=1; fi
 done
 
 echo "====  TEST END  ====="
