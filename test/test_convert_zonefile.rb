6a3a17a535d4f94d642caaa07e1192feac6199ab
Satoshi Tajima <Satoshi_Tajima@voyagegroup.com>
2014-01-15 16:59:35 +0900

    fix configfile config

--- test/test_convert_zonefile.rb
+++ test/test_convert_zonefile.rb
@@ -8,33 +8,37 @@ class TestConvertZonefile < Test::Unit::TestCase
   end
 
   def test_zonename
+    configfile_path = './config/default.yml'
+  
     zonefile_path  = './zonefile_for_test/test_convert_zonefile_zonename.zone'
     zonename       = 'example.com'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     assert_equal true,  ConvertZonefile.new(args).template_hash["Resources"].include?("R53ExampleCom")
 
     zonefile_path = './zonefile_for_test/test_convert_zonefile_zonename.zone'
     zonename      = 'example.com.'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     assert_equal true,  ConvertZonefile.new(args).template_hash["Resources"].include?("R53ExampleCom")
 
     zonefile_path = './zonefile_for_test/test_convert_zonefile_zonename.zone'
     zonename      = '0/25.example.com.'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     assert_equal true,  ConvertZonefile.new(args).template_hash["Resources"].include?('R53025ExampleCom')
 
     zonefile_path = './zonefile_for_test/test_convert_zonefile_zonename.zone'
     zonename      = 'example.com.'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     assert_equal false,  ConvertZonefile.new(args).template_hash["Resources"].include?("example.com.")
   end
 
   def test_a_record
+    configfile_path = './config/default.yml'
+  
     zonename      = 'example.com.'
     resource_name = zonename2resourcename(zonename, 'R53')
 
     zonefile_path  = './zonefile_for_test/test_convert_zonefile_a_record01.zone'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     recordsets = ConvertZonefile.new(args).template_hash["Resources"][resource_name]["Properties"]["RecordSets"]
     expects = [{"ResourceRecords"=>["192.168.4.1"], "TTL"=>"600", "Name"=>"example.com.",      "Type"=>"A"},
                {"ResourceRecords"=>["192.168.4.1"], "TTL"=>"100", "Name"=>"test.example.com.", "Type"=>"A"}]
@@ -42,7 +46,7 @@ class TestConvertZonefile < Test::Unit::TestCase
     assert_equal [], recordsets - expects
 
     zonefile_path = './zonefile_for_test/test_convert_zonefile_a_record02.zone'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     recordsets = ConvertZonefile.new(args).template_hash["Resources"][resource_name]["Properties"]["RecordSets"]
     expects = [{"ResourceRecords"=> ["192.168.4.1", "192.168.4.2"], "TTL"=>"900", "Name"=>"example.com.", "Type"=>"A"}]
     assert_equal [], expects - recordsets
@@ -50,11 +54,13 @@ class TestConvertZonefile < Test::Unit::TestCase
   end
 
   def test_cname_record
+    configfile_path = './config/default.yml'
+
     zonename      = 'example.com.'
     resource_name = zonename2resourcename(zonename, 'R53')
 
     zonefile_path = './zonefile_for_test/test_convert_zonefile_cname_record01.zone'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     recordsets = ConvertZonefile.new(args).template_hash["Resources"][resource_name]["Properties"]["RecordSets"]
     expects = [{"ResourceRecords"=>["192.168.4.1"],       "TTL"=>"700", "Name"=>"test.example.com.",     "Type"=>"A"},
                {"ResourceRecords"=>["test.example.com."], "TTL"=>"700", "Name"=>"cname.example.com.",    "Type"=>"CNAME"},
@@ -65,11 +71,13 @@ class TestConvertZonefile < Test::Unit::TestCase
   end
 
   def test_mx_record
+    configfile_path = './config/default.yml'
+
     zonename      = 'example.com.'
     resource_name = zonename2resourcename(zonename, 'R53')
 
     zonefile_path = './zonefile_for_test/test_convert_zonefile_mx_record01.zone'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     recordsets = ConvertZonefile.new(args).template_hash["Resources"][resource_name]["Properties"]["RecordSets"]
     expects = [{"ResourceRecords"=>["192.168.4.1"],           "TTL"=>"900", "Name"=>"mail2.example.com.", "Type"=>"A"},
                {"ResourceRecords"=>["10 mail.other.com."],    "TTL"=>"900", "Name"=>"mail.example.com.", "Type"=>"MX"},
@@ -79,11 +87,13 @@ class TestConvertZonefile < Test::Unit::TestCase
   end
 
   def test_txt_record
+    configfile_path = './config/default.yml'
+
     zonename      = 'example.com.'
     resource_name = zonename2resourcename(zonename, 'R53')
 
     zonefile_path = './zonefile_for_test/test_convert_zonefile_txt_record01.zone'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     recordsets = ConvertZonefile.new(args).template_hash["Resources"][resource_name]["Properties"]["RecordSets"]
     expects = [{"ResourceRecords"=>["\"v=spf1 +ip4:192.168.4.0/24 +ip4:10.0.0.0/24 ~all\""], "TTL"=>"900", "Name"=>"spf.example.com.",  "Type"=>"TXT"},
                {"ResourceRecords"=>["\"KdeSEn375EyXno10f2izhAp04ENSXW0LitQBYKJAG/c=\""],     "TTL"=>"100", "Name"=>"spf2.example.com.", "Type"=>"TXT"},
@@ -94,11 +104,13 @@ class TestConvertZonefile < Test::Unit::TestCase
   end
 
   def test_ptr_record
+    configfile_path = './config/default.yml'
+
     zonename      = '4.168.192.in-addr.arpa.'
     resource_name = zonename2resourcename(zonename, 'R53')
 
     zonefile_path = './zonefile_for_test/test_convert_zonefile_ptr_record01.zone'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     recordsets = ConvertZonefile.new(args).template_hash["Resources"][resource_name]["Properties"]["RecordSets"]
     expects = [{"ResourceRecords"=>["test10.example.com."], "TTL"=>"900", "Name"=>"10.4.168.192.in-addr.arpa.", "Type"=>"PTR"},
                {"ResourceRecords"=>["test20.example.com."], "TTL"=>"100", "Name"=>"20.4.168.192.in-addr.arpa.", "Type"=>"PTR"}]
@@ -109,17 +121,19 @@ class TestConvertZonefile < Test::Unit::TestCase
     resource_name = zonename2resourcename(zonename, 'R53')
 
     zonefile_path = './zonefile_for_test/test_convert_zonefile_ptr_record02.zone'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     hosted_zonename = ConvertZonefile.new(args).template_hash["Resources"][resource_name]["Properties"]["HostedZoneName"]
     assert_equal '0\05725.4.168.192.in-addr.arpa.', hosted_zonename
   end
 
   def test_ns_record
+    configfile_path = './config/default.yml'
+
     zonename      = 'example.com.'
     resource_name = zonename2resourcename(zonename, 'R53')
 
     zonefile_path  = './zonefile_for_test/test_convert_zonefile_ns_record01.zone'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     recordsets = ConvertZonefile.new(args).template_hash["Resources"][resource_name]["Properties"]["RecordSets"]
     expects = [{"ResourceRecords"=>["ns-test1-1.example.com.", "ns-test1-2.example.com."], "TTL"=>"900", "Name"=>"ns-test1.example.com.", "Type"=>"NS"},
                {"ResourceRecords"=>["ns-test2-1.example.com.", "ns-test2-2.example.com."], "TTL"=>"200", "Name"=>"ns-test2.example.com.", "Type"=>"NS"}]
@@ -128,11 +142,13 @@ class TestConvertZonefile < Test::Unit::TestCase
   end
 
   def test_aws_specific
+    configfile_path = './config/default.yml'
+
     zonename      = 'example.com.'
     resource_name = zonename2resourcename(zonename, 'R53')
 
     zonefile_path  = './zonefile_for_test/test_convert_zonefile_aws_specific.zone'
-    args = ['-f', zonefile_path, '-z', zonename]
+    args = ['-f', zonefile_path, '-z', zonename, '-c', configfile_path]
     recordsets = ConvertZonefile.new(args).template_hash["Resources"][resource_name]["Properties"]["RecordSets"]
     expects = [
       {"ResourceRecords"=>["192.168.4.1"], "TTL"=>"900", "Name"=>"test.example.com.", "Type"=>"A"},
